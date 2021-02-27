4596522c5ee1f649e34b17ce9fdf7e499bc4be55
jingmeimei <jingmeimei@kuaishou.com>
2021-02-27 22:31:50 +0800

    样式修改

--- management/src/view/homePage/FileTable.vue
+++ management/src/view/homePage/FileTable.vue
@@ -20,7 +20,7 @@
         :pageNo="pageNo"
         :pageSize="pageSize"
       >
-        <el-table-column label="操作">
+        <el-table-column label="操作" width="240">
           <template slot-scope="scope">
             <el-button size="mini" type="primary" @click="preview(scope.row)"
               >预览</el-button
--- management/src/view/homePage/UserManagement.vue
+++ management/src/view/homePage/UserManagement.vue
@@ -20,7 +20,7 @@
         :pageNo="pageNo"
         :pageSize="pageSize"
       >
-        <el-table-column label="操作">
+        <el-table-column label="操作" width="260">
           <template slot-scope="scope">
             <el-button
               size="mini"
